bbc580e22419878bcdd0368daae30e13452a8d21
renedeanda <rene.ideanda@gmail.com>
2021-04-23 11:43:47 -0500

    types error

--- components/linkButtons.tsx
+++ components/linkButtons.tsx
@@ -10,58 +10,41 @@ export default function LinkButtons(
   }) {
 
   const buttons =
-    (portfolio && portfolio.links ?
-      <List>
-        {portfolio.links.blog ?
-          <Button
-            as='a'
-            size={size}
-            style={{ display: 'inline-block', margin: '0.2em' }}
-            href={withHttp(portfolio.links.blog)}
-            target="_blank"
-            icon='rss'
-            circular
-            onClick={(e) => {
-              e.stopPropagation();
-            }} />
-          : null}
-        {portfolio.links.linkedin ?
-          <Button
-            as='a'
-            size={size}
-            style={{ display: 'inline-block', margin: '0.2em' }}
-            href={withHttp(portfolio.links.linkedin)}
-            target="_blank"
-            icon='linkedin'
-            color='linkedin'
-            circular
-            onClick={(e) => {
-              e.stopPropagation();
-            }} />
-          : null}
-      </List>
-      : null)
-
-  const textList = (
-    (portfolio && portfolio.links ?
-      <List link style={{ fontSize: '1.33em' }}>
-        <List.Item
-          href={withHttp(portfolio.links.website)}
-          target='_blank'><Icon name='linkify' />{portfolio.links.website}</List.Item>
-        {portfolio.links.linkedin ?
-          <List.Item
-            href={withHttp(portfolio.links.linkedin)}
-            target='_blank'><Icon name='linkedin' />LinkedIn</List.Item>
-          : null}
-        {portfolio.links.blog ?
-          <List.Item
-            href={withHttp(portfolio.links.blog)}
-            target='_blank'><Icon name='rss' />Blog</List.Item>
-          : null}
-      </List>
-      : null))
+    // (portfolio && portfolio.links ?
+    //   <List>
+    //     {portfolio.links.blog ?
+    //       <Button
+    //         as='a'
+    //         size={size}
+    //         style={{ display: 'inline-block', margin: '0.2em' }}
+    //         href={withHttp(portfolio.links.blog)}
+    //         target="_blank"
+    //         icon='rss'
+    //         circular
+    //         onClick={(e) => {
+    //           e.stopPropagation();
+    //         }} />
+    //       : null}
+    //     {portfolio.links.linkedin ?
+    //       <Button
+    //         as='a'
+    //         size={size}
+    //         style={{ display: 'inline-block', margin: '0.2em' }}
+    //         href={withHttp(portfolio.links.linkedin)}
+    //         target="_blank"
+    //         icon='linkedin'
+    //         color='linkedin'
+    //         circular
+    //         onClick={(e) => {
+    //           e.stopPropagation();
+    //         }} />
+    //       : null}
+    //   </List>
+    //   : 
+      null
+      // )
 
   return (
-    isTextList ? textList : buttons
+    buttons
   )
 }
