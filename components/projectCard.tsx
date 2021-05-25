d5c2cf06989e69f4d5103900c6eea0ed3545c49c
renedeanda <rene.ideanda@gmail.com>
2021-05-25 15:18:31 -0500

    update project card design, update roles model in portfolio

--- components/projectCard.tsx
+++ components/projectCard.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import { Card } from 'semantic-ui-react';
+import { Card, Label } from 'semantic-ui-react';
 import { Project } from '../types/project.types';
 
 export default function ProjectCard({ project }: {
@@ -14,11 +14,22 @@ export default function ProjectCard({ project }: {
         fluid
         style={{ boxShadow: '0 0 30px 0 rgb(0 0 0 / 12%)', borderRadius: '8px', padding: '22px', marginTop: '30px' }}>
         <Card.Content textAlign='left'>
-          <h2 className='card-title' style={{ marginTop: 0, marginBottom: '8px' }}>{project.title}</h2>
-          <p style={{ fontSize: '1.1em', marginTop: '8px' }} className='tagline'>
+          <h2 className='card-title' style={{ marginTop: 0, marginBottom: '16px' }}>{project.title}</h2>
+          <p style={{ fontSize: '1.22em', marginTop: '8px' }} className='tagline'>
             {project.summary
               ? project.summary : null}
           </p>
+          {project.roles && project.roles.length > 0
+            ?
+            project.roles.map((role: string, key) =>
+              <Label
+                key={key}
+                size='large'
+                className='role-label'
+                style={{ display: 'inline-block', margin: '0.4em 0.4em 0.4em 0', padding: '0.5em' }}>
+                {role}
+              </Label>)
+            : null}
         </Card.Content>
       </Card >
     </Link>
