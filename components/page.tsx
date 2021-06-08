dfdaedef797099efe38d81020f1a96c91aca4e24
renedeanda <rene.ideanda@gmail.com>
2021-06-08 12:20:33 -0500

    add /projects & /highlights to mobile sidebar

--- components/page.tsx
+++ components/page.tsx
@@ -37,6 +37,22 @@ export default function Page({ children, portfolio }: {
                 <Icon name='lightbulb outline' />About Me</div>
             </Link>
           </Menu.Item>
+          {portfolio.projects && portfolio.projects.length > 0 ?
+            <Menu.Item as='a'>
+              <Link href='/projects'>
+                <div style={{ padding: '0.5em' }} className='sidebar-text'>
+                  <Icon name='hammer' />Projects</div>
+              </Link>
+            </Menu.Item>
+            : null}
+          {portfolio.highlights && portfolio.highlights.length > 0 ?
+            <Menu.Item as='a'>
+              <Link href='/highlights'>
+                <div style={{ padding: '0.5em' }} className='sidebar-text'>
+                  <Icon name='star outline' />Highlights</div>
+              </Link>
+            </Menu.Item>
+            : null}
           {portfolio.blog ?
             <Menu.Item
               as='a'
