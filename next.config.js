ecf567011c1c6a7531393831d52aaf8cdb7afa73
renedeanda <rene.ideanda@gmail.com>
2021-12-18 17:37:20 -0800

    canvas

--- next.config.js
+++ next.config.js
@@ -1,13 +1,5 @@
 module.exports = {
     webpack: function (config, { isServer }) {
-        if (
-            process.env.LD_LIBRARY_PATH == null ||
-            !process.env.LD_LIBRARY_PATH.includes(
-                `${process.env.PWD}/node_modules/canvas/build/Release:`,
-            )
-        ) {
-            process.env.LD_LIBRARY_PATH = `${process.env.PWD}/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
-        }
         config.module.rules.push({
             test: /\.md$/,
             use: 'raw-loader',
