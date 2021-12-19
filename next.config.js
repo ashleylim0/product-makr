39b50190be2c4fdb3f7a5168f769969aaf0e3f64
renedeanda <rene.ideanda@gmail.com>
2021-12-18 17:24:53 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,3 @@
-if (
-    process.env.LD_LIBRARY_PATH == null ||
-    !process.env.LD_LIBRARY_PATH.includes(
-        `${process.env.PWD}/node_modules/canvas/build/Release:`,
-    )
-) {
-    process.env.LD_LIBRARY_PATH = `${process.env.PWD}/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
-}
-
 module.exports = {
     webpack: function (config, { isServer }) {
         config.module.rules.push({
@@ -16,6 +7,14 @@ module.exports = {
         if (isServer) {
             import('./util/generateSiteMap.mjs')
             import('./util/generateShareFavImages.mjs')
+        }
+        if (
+            process.env.LD_LIBRARY_PATH == null ||
+            !process.env.LD_LIBRARY_PATH.includes(
+                `${process.env.PWD}/node_modules/canvas/build/Release:`,
+            )
+        ) {
+            process.env.LD_LIBRARY_PATH = `${process.env.PWD}/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
         }
         return config
     }
