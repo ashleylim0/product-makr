92613ea8eabdd8b839e1d3ff6bf939237a0c2283
renedeanda <rene.ideanda@gmail.com>
2022-02-06 20:57:16 -0800

    Revert canvas version - still not fixed

--- next.config.js
+++ next.config.js
@@ -5,16 +5,6 @@ module.exports = {
             use: 'raw-loader',
         })
         if (isServer) {
-            // For building on vercel: https://github.com/Automattic/node-canvas/issues/1779
-            if (
-                process.env.LD_LIBRARY_PATH == null ||
-                !process.env.LD_LIBRARY_PATH.includes(
-                    `${process.env.PWD}/node_modules/canvas/build/Release:`,
-                )
-            ) {
-                process.env.LD_LIBRARY_PATH = `${process.env.PWD
-                    }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
-            }
             import('./util/generateSiteMap.mjs')
             import('./util/generateShareFavImages.mjs')
         }
