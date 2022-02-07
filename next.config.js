673368e08f1281765ecd2eeed93822e6d1c1257d
renedeanda <rene.ideanda@gmail.com>
2022-02-06 20:42:17 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,20 +1,20 @@
 module.exports = {
     webpack: function (config, { isServer }) {
-        // For building on vercel: https://github.com/Automattic/node-canvas/issues/1779
-        if (
-            process.env.LD_LIBRARY_PATH == null ||
-            !process.env.LD_LIBRARY_PATH.includes(
-                `${process.env.PWD}/node_modules/canvas/build/Release:`,
-            )
-        ) {
-            process.env.LD_LIBRARY_PATH = `${process.env.PWD
-                }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
-        }
         config.module.rules.push({
             test: /\.md$/,
             use: 'raw-loader',
         })
         if (isServer) {
+            // For building on vercel: https://github.com/Automattic/node-canvas/issues/1779
+            if (
+                process.env.LD_LIBRARY_PATH == null ||
+                !process.env.LD_LIBRARY_PATH.includes(
+                    `${process.env.PWD}/node_modules/canvas/build/Release:`,
+                )
+            ) {
+                process.env.LD_LIBRARY_PATH = `${process.env.PWD
+                    }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
+            }
             import('./util/generateSiteMap.mjs')
             import('./util/generateShareFavImages.mjs')
         }
